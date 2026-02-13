727282d1fa2408ec15cfb7cd4bc76fe428e52d4f
htethlaingwin <htethlaingwin.bsn29@gmail.com>
2026-02-13 20:18:17 +0630

    goose

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"errors"
-	"fmt"
 	"log"
 	"os"
 
@@ -13,76 +11,30 @@ type state struct {
 	cfg *config.Config
 }
 
-type command struct {
-	name string
-	args []string
-}
-
-func handlerLogin(s *state, cmd command) error {
-	if cmd.args == nil {
-		return errors.New("no argument pass for login")
-	}
-	err := s.cfg.SetUser(cmd.args[0])
-	if err != nil {
-		return err
-	}
-	fmt.Printf("User %s is login\n", s.cfg.CurrentUsername)
-	return nil
-}
-
-type commands struct {
-	handlerFuncs map[string]func(*state, command) error
-}
-
-func (c *commands) run(s *state, cmd command) error {
-	handler, ok := c.handlerFuncs[cmd.name]
-	if !ok {
-		return errors.New("the given handler is not registered")
-	}
-	return handler(s, cmd)
-}
-
-func (c *commands) register(name string, f func(*state, command) error) {
-	c.handlerFuncs[name] = f
-}
-
 func main() {
-	args := os.Args
-	if len(args) < 2 {
-		log.Fatal("not enough arguments were provided")
-	}
-
-	if args[1] == "login" && len(args) < 3 {
-		log.Fatal("a username is required")
-	}
-
-	var s state
 	cfg, err := config.Read()
 	if err != nil {
 		log.Fatalf("error reading config: %v", err)
 	}
-	fmt.Printf("Reading config: %+v\n", cfg)
 
-	s.cfg = &cfg
+	programState := &state{
+		cfg: &cfg,
+	}
 
 	cmds := commands{
-		handlerFuncs: make(map[string]func(*state, command) error),
+		registeredCommands: make(map[string]func(*state, command) error),
 	}
 	cmds.register("login", handlerLogin)
 
-	cmd := command{
-		name: args[1],
-		args: []string{args[2]},
+	if len(os.Args) < 2 {
+		log.Fatal("Usage: cli <command> [args...]")
 	}
 
-	err = cmds.run(&s, cmd)
-	if err != nil {
-		log.Fatalf("cannot run command: %v", err)
-	}
+	cmdName := os.Args[1]
+	cmdArgs := os.Args[2:]
 
-	cfg, err = config.Read()
+	err = cmds.run(programState, command{Name: cmdName, Args: cmdArgs})
 	if err != nil {
-		log.Fatalf("error reading config: %v", err)
+		log.Fatal(err)
 	}
-	fmt.Printf("Reading config again: %+v\n", cfg)
 }
---
+++ sql/schema/001_users.sql
@@ -0,0 +1,11 @@
+-- +goose up
+CREATE TABLE users (
+  id UUID PRIMARY KEY,
+  created_at TIMESTAMP,
+  updated_at TIMESTAMP,
+  name TEXT UNIQUE NOT NULL
+);
+
+
+-- +goose Down
+DROP TABLE users;
